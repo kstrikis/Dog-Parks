db206219c24af199ee5ea21cabd21c453b827e43
Alex Nunan <nunanamous21@gmail.com>
2023-05-09 15:03:27 -0400

    Adding paws to display rating

--- client/src/components/ReviewTile.js
+++ client/src/components/ReviewTile.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
+
 
 const ReviewTile = ({ reviewText, userName, rating }) => {
+
+    const checkedIcon = <FontAwesomeIcon className="right-margin-smaller" icon={faPawSolid} />
+    const uncheckedIcon = <FontAwesomeIcon className="right-margin-smaller" icon={faPawRegular} />
+
+    let iconRatingArray = []
+    const getRating = (rating) => {
+        for(let i = 1; i<=5; i++){
+            if(i<=rating){
+                iconRatingArray.push(checkedIcon)
+            } else {
+                iconRatingArray.push(uncheckedIcon)
+            }
+        }
+        return iconRatingArray
+    }
+
     return (
         <>
             <h4>{userName}'s Review</h4>
             <p>{reviewText}</p>
-            <p>Rating: {rating}</p>
+            <p>Rating: {getRating(rating)}</p>
         </>
     )
 }
